14d07d23127830ffdb590a375c4e9b056e0ea9d7
Ian Prentice <29048123+itprentice@users.noreply.github.com>
2019-12-30 21:16:20 -0500

    Takes user input of make, gives users options based on the make input, and outputs the enedc. (Fake Dictionary Though)

---
+++ AnyPhallicMember/background.js
@@ -0,0 +1,10 @@
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {pathPrefix: '/maps'}
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
